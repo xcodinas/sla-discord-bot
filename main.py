808ee6a531053d8892898deaecf29a732f418ece
Xavier Codinas <xavier19966@gmail.com>
2024-05-18 19:31:09 +0200

    Improve code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from googletrans import Translator
 import cv2
 import pytesseract
-import re
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# Path to the tesseract executable if on Windows
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 next_tier = 'img/total_power_required.png'
+image_path = 'img/image_es.png'
+template_path = 'img/max_total_power.png'
 
 translator = Translator()
 
@@ -19,18 +21,25 @@ def check_sidebar(image_path):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Template matching to find the region of interest
-    result = cv2.matchTemplate(gray_image, sidebar_template, cv2.TM_CCOEFF_NORMED)
+    result = cv2.matchTemplate(
+            gray_image,
+            sidebar_template,
+            cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
     # Get the bounding box for the detected region
     template_height, template_width = sidebar_template.shape
     top_left = max_loc
-    bottom_right = (top_left[0] + template_width, top_left[1] + template_height)
+    bottom_right = (top_left[0] + template_width,
+                    top_left[1] + template_height)
 
     # Check the text in the detected region and translate it
     roi = gray_image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-    _, thresh_roi = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    extracted_text = translator.translate(pytesseract.image_to_string(thresh_roi), src='auto', dest='en').text
+    _, thresh_roi = cv2.threshold(roi, 0, 255,
+                                  cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    extracted_text = translator.translate(
+            pytesseract.image_to_string(thresh_roi), src='auto', dest='en'
+            ).text
 
     if "achievement reward" in extracted_text.lower():
         return True, bottom_right[0] - top_left[0]
@@ -51,10 +60,9 @@ def extract_numbers_with_template(image_path, template_path):
     sidebar, sidebar_width = check_sidebar(image_path)
     if sidebar:
         # Crop the image to exclude the sidebar from the right
-        gray_image = gray_image[:, :gray_image.shape[1] - (sidebar_width + 300)]
+        gray_image = gray_image[:, :gray_image.shape[1] - (
+            sidebar_width + 300)]
         thresh = thresh[:, :thresh.shape[1] - (sidebar_width + 300)]
-
-    cv2.imwrite('thresh.png', thresh)
 
     result = cv2.matchTemplate(gray_image, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
@@ -62,21 +70,19 @@ def extract_numbers_with_template(image_path, template_path):
     # Get the bounding box for the detected region
     template_height, template_width = template.shape
     top_left = max_loc
-    bottom_right = (top_left[0] + template_width, top_left[1] + template_height)
+    bottom_right = (
+            top_left[0] + template_width, top_left[1] + template_height)
 
     # Extract the region of interest (ROI) around the detected template
     roi = thresh[top_left[1]:bottom_right[1],
-          top_left[0]:bottom_right[0] + 150]  # Extend width to include the numbers
-
-    cv2.imwrite('roi.png', roi)
+                 top_left[0]:bottom_right[0] + 150
+                 ]
 
     # Use pytesseract to extract text from the thresholded ROI
     custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789'
     extracted_text = pytesseract.image_to_string(roi, config=custom_config)
     return extracted_text
 
-# TODO: ADD tests optimize waith chatgpt
-image_path = 'img/image_es.png'
-template_path = 'img/max_total_power.png'
+
 numbers = extract_numbers_with_template(image_path, template_path)
 print(f"Extracted Numbers: {numbers}")
